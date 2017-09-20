326afba751cbb919426191f55f7e44e1c793a8b1
allycs <allycs@126.com>
2017-09-20 18:00:10 +0800

    增加连接管理类增加连接关闭方法

---
+++ SharedLibrary/ConnectionManage.cs
@@ -0,0 +1,23 @@
+﻿namespace Dapper
+{
+    using System.Data;
+
+    /// <summary>
+    /// 数据库连接管理类
+    /// </summary>
+    public static class ConnectionManage
+    {
+        /// <summary>
+        /// 关闭当前连接
+        /// </summary>
+        /// <param name="connection"></param>
+        public static void ConnClose(this IDbConnection connection)
+        {
+            if (connection != null && connection.State != ConnectionState.Closed)
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+    }
+}
