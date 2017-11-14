51c9b0dd5569966f42e6980b262792dd6b3b74f1
allycs <allycs@126.com>
2017-11-14 09:58:57 +0800

    整理代码

--- Demo_DotNetCore/Program.cs
+++ Demo_DotNetCore/Program.cs
@@ -3,13 +3,11 @@ using System;
 
 namespace Demo_DotNetCore
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var token = "token";
-            var userId = "userId";
-            var members = DbHelper.CreateConnection().GetList<Member>(new { token="Token",uid="UserId"},"dt_customer");
+            var members = DbHelper.CreateConnection().GetList<Member>(new { token = "Token", uid = "UserId" }, "dt_customer");
             foreach (var item in members)
             {
                 Console.WriteLine(item.Nickname);
