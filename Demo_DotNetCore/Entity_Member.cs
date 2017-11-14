728167e0fd7fb5720dd836df4e70cb91e1b5c452
allycs <allycs@126.com>
2017-11-14 09:56:38 +0800

    增加DotNetCore使用的Demo

---
+++ Demo_DotNetCore/Entity_Member.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_DotNetCore
+{
+    class Entity_Member
+    {
+    }
+}
---
+++ Demo_DotNetCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System;
+
+namespace Demo_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = "token";
+            var userId = "userId";
+            var members = DbHelper.CreateConnection().GetList<Member>("dt_customer", new { token = token, uid = userId });
+            foreach (var item in members)
+            {
+                Console.WriteLine(item.Nickname);
+            }
+            Console.ReadLine();
+        }
+    }
+}
